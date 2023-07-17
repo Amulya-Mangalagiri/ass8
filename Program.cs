a8ad639390dee78b3d20ec0389f41aef941bedb8
Amulya-Mangalagiri <139097211+Amulya-Mangalagiri@users.noreply.github.com>
2023-07-17 15:39:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,76 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Assignment8
 {
-    class Salary
-    {
-        public double calc_salary(double wHour, double wDays, double projectHandle = 1, double extras = 0)
-        {
-            double sal = wHour * wDays * 100 + projectHandle * 3000 + extras * 2000;
 
-            return sal;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Salary salary = new Salary();
-            Console.WriteLine("HR: " + salary.calc_salary(8, 30));
-            Console.WriteLine("Admin: " + salary.calc_salary(8, 30, 2));
-            Console.WriteLine("Software Developer: " + salary.calc_salary(8, 30, 3, 1));
-            Console.ReadKey();
+            Console.WriteLine("Choose Employee Type:");
+            Console.WriteLine("1. HR");
+            Console.WriteLine("2. Admin");
+            Console.WriteLine("3. Software Developer");
+
+            Console.Write("Enter the employee type (1-3): ");
+            int employeeType = int.Parse(Console.ReadLine());
+            int wHour, nWDays, projectHandles, extras;
+            double monthlySalary;
+
+            switch (employeeType)
+            {
+                case 1: // HR
+                    Console.Write("Enter working hours: ");
+                    wHour = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    nWDays = int.Parse(Console.ReadLine());
+                    monthlySalary = CalculateSalary(1, 1);
+                    Console.WriteLine("Monthly Salary: $" + monthlySalary);
+                    break;
+
+                case 2: // Admin
+                    Console.Write("Enter working hours: ");
+                    wHour = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    nWDays = int.Parse(Console.ReadLine());
+                    Console.Write("Enter project handles: ");
+                    projectHandles = int.Parse(Console.ReadLine());
+
+                    monthlySalary = CalculateSalary(1, 1, 1);
+                    Console.WriteLine("Monthly Salary: $" + monthlySalary);
+                    break;
+
+                case 3: // Software Developer
+                    Console.Write("Enter working hours: ");
+                    wHour = int.Parse(Console.ReadLine());
+                    Console.Write("Enter number of working days: ");
+                    nWDays = int.Parse(Console.ReadLine());
+                    Console.Write("Enter project handles: ");
+                    projectHandles = int.Parse(Console.ReadLine());
+                    Console.Write("Enter extras: ");
+                    extras = int.Parse(Console.ReadLine());
+
+                    monthlySalary = CalculateSalary(1, 1, 1, 1);
+                    Console.WriteLine("Monthly Salary: $" + monthlySalary);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid employee type!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        static double CalculateSalary(int wHour, int nWDays, int projectHandles = 0, int extras = 0)
+        {
+            double total = wHour * nWDays * 100 + projectHandles * 3000 + extras * 2000;
+            return total;
         }
     }
 }
